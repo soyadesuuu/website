aa19c3a17ef5dc15c5da1e5bcbebc8df49f9f8dd
April <april@dummy.cafe>
2024-10-28 01:35:38 +0100

    Add button.js

---
+++ scripts/button.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const button = document.getElementById('tuah');
+    const socket = new WebSocket('wss://api.soya.place/button');
+
+    socket.addEventListener('open', () => {
+        button.disabled = false;
+    });
+
+    button.addEventListener('click', () => {
+        if (socket.readyState == socket.OPEN) {
+            socket.send({type:0});
+            button.disabled = true;
+            setTimeout(() => { button.disabled = false; }, 1000);
+        } else button.disabled = true;
+    });
+}
