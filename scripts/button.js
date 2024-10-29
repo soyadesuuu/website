6cb45435070abb6e8199c7243e998bad1bb4d3df
April <april@dummy.cafe>
2024-10-30 00:10:57 +0100

    get countdown from server

--- scripts/button.js
+++ scripts/button.js
@@ -6,11 +6,28 @@ window.onload = () => {
         button.disabled = false;
     });
 
+    let countdown = 1000;
+    socket.addEventListener('message', () => {
+        try {
+            const data = JSON.parse(event.data);
+            if (data.type != undefined) {
+                switch (data.type) {
+                    case 0: { // ButtonResponseType.SetCountdown
+                        countdown = data.value;
+                    }
+                }
+            }
+        } catch(err) { console.error(err); }
+    });
+
     button.addEventListener('click', () => {
         if (socket.readyState == socket.OPEN) {
             socket.send(JSON.stringify({type:0}));
             button.disabled = true;
-            setTimeout(() => { button.disabled = false; }, 1000);
-        } else button.disabled = true;
+            setTimeout(() => { button.disabled = false; }, countdown);
+        } else {
+            button.disabled = true;
+            button.textContent = 'Disconnected. Please refresh to continue spitting on that thang';
+        }
     });
 }
